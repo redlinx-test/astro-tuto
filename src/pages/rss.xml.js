8b1e60aea02c63ffb920434e8c643a661ecc326a
Xav <contact@redlinx.fr>
2024-12-26 21:05:45 +0100

    debug rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,6 +2,7 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+    const posts = await getCollection("blog");
     return rss({
         title: 'Astro tuto test | Blog',
         description: "Mon voyage d'apprentissage d'Astro, c'est cool",
